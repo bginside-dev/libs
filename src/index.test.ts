6eb8adc7de6493b4a560d13a8602b050433ebefb
Bludvikk <cleverdmasoy@gmail.com>
2023-09-28 13:13:23 +0800

    yesser

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,6 @@ import { describe, expect, it } from 'vitest'
 
 describe("Empire Query", () => {
     it("should create empire-query", () => {
-        expect(2).toBe(2);
+        expect(1).toBe(1);
     })
 })
